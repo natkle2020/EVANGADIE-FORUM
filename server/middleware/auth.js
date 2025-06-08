be4ad777838e350e16990dcf8366aff80b69fb23
Bewket Wondim <bwondim2014@gmail.com>
2025-06-08 03:23:31 -0400

    initial commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,24 +2,24 @@ import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
 
 async function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
+const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
+if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ msg: "Authentication invalid" });
-  }
-  const token = authHeader.split(" ")[1];
+    .status(StatusCodes.UNAUTHORIZED)
+    .json({ msg: "Authentication invalid" });
+}
+const token = authHeader.split(" ")[1];
 
-  try {
+try {
     const { username, user_id } = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { username, user_id };
     next();
-  } catch (error) {
+} catch (error) {
     return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ msg: "Authentication invalid" });
-  }
+    .status(StatusCodes.UNAUTHORIZED)
+    .json({ msg: "Authentication invalid" });
+}
 }
 
 export default authMiddleware;
