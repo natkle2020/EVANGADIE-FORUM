c607c1b6c58821ba270e8f57fb651ac9aedfcc9c
natkle2020 <natkle2020@gmail.com>
2025-06-28 22:41:10 +0300

    done

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -1,29 +1,17 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 dotenv.config();
 const dbconfig = {
-<<<<<<< HEAD
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '4000',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD  ,
-    database: process.env.DB_NAME || 'evangadi_networks',
+    database: process.env.DB_NAME || 'evangadi_network',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 
 }
-=======
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-};
->>>>>>> befc4e45b87360b05a7c0825612566659a975f85
 
 // Creating connection pool
 const pool = mysql.createPool(dbconfig);
