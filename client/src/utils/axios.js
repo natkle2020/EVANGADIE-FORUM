9109cb66fe2db3edc857adea35ff0ecbc6301da0
natkle2020 <natkle2020@gmail.com>
2025-06-28 23:52:50 +0300

    done

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3200/api",
-  baseURL: "https://evangadie-forum.onrender.com"
+  baseURL: "https://evangadie-forum.onrender.com/api"
 });
 
 export default axiosInstance;
